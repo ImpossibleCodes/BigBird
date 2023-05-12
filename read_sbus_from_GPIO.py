94cddd39745f6cee1fdad3cb04c8949deb4bb5b9
Neil Agrawal <neil.agrawal6@gmail.com>
2023-05-12 09:36:46 -0400

    Invert

--- read_sbus_from_GPIO.py
+++ read_sbus_from_GPIO.py
@@ -167,8 +167,8 @@ class SbusReader:
         #skip first frame, it is an SBUS start frame
         for packet_bits_ptr in range (_UART_FRAME_LENGTH,_UART_FRAME_LENGTH+22*_UART_FRAME_LENGTH,_UART_FRAME_LENGTH):
             #extract from UART frame and invert each byte
-            #edited to not invert, since we are using already inverted logic with transistor between receiver and pi, to reapply inversion add "~"
-            channel_bits[channel_bits_ptr:channel_bits_ptr+8]=packet[packet_bits_ptr+1:packet_bits_ptr+9]
+            #NVM ignore this: edited to not invert, since we are using already inverted logic with transistor between receiver and pi, to reapply inversion add "~"
+            channel_bits[channel_bits_ptr:channel_bits_ptr+8]=~packet[packet_bits_ptr+1:packet_bits_ptr+9]
             channel_bits_ptr += 8
 
         ret_list = []
